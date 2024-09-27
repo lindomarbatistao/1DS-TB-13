7d86e43c97c640b99aaae5ed14efd49ea3adf6b9
Catifundo <lindomarbatistao@gmail.com>
2024-09-27 14:41:54 -0300

    27-09-24 v1

--- api/models.py
+++ api/models.py
@@ -4,9 +4,9 @@ class Produto(models.Model):
     codigoProduto = models.IntegerField(max_length=10)
     tituloProduto = models.CharField(max_length=255)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    descricao = models.TextField(max_length=500)    
-# 	descricao (str): Descrição detalhada do produto.
-# 	imagemProduto (str): Caminho ou URL da imagem do produto.
-# 	categoriaProduto (str): Categoria à qual o produto pertence.
-# 	classificacaoProduto (float): Classificação média do produto com base nas avaliações dos usuários.
-# 	exibirHome (bool): Indica se o produto deve ser exibido na página inicial.
+    descricao = models.TextField(max_length=500) 
+    imagemProduto = models.CharField(max_length=255)
+    categoriaProduto = models.CharField(max_length=255)
+    classProduto = models.CharField(max_length=255)
+    exibirHome = models.BooleanField(default=True)
+
