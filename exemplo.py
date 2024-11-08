5ea0c1e6e2ace10326b6705b1992d6379339a40f
Catifundo <lindomarbatistao@gmail.com>
2024-11-08 16:57:04 -0300

    08-11-24

--- exemplo.py
+++ exemplo.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 class Ecommerce:
     def __init__ (self, db='db.sqlite3'):
@@ -19,9 +20,14 @@ class Ecommerce:
                 case 1:
                     self.menu_create()
                 case 2:
-                    print('Read')
+                    self.read()
                 case 3:
-                    print('Update')
+                    df = pd.read_sql_query('SELECT * FROM api_produto', self.conn)
+                    print(df)
+                    valor = int(input('ID: '))
+                    query = f'SELECT * FROM api_produto WHERE id = {valor}'
+                    df = pd.read_sql_query(query, self.conn)
+                    print(df)
                 case 4:
                     print('Delete')
                 case 5:
@@ -61,6 +67,63 @@ class Ecommerce:
         exibirHome = input("Exibir(True, False): ")
         self.create(titulo, preco, descricao, imagemProduto,
                categoriaProduto, classProduto, exibirHome)
+
+    def read(self):
+        print('\n'
+              '[1]-Listar todos os produtos\n'
+              '[2]-Listar por ID'
+              )
+        op = int(input("Escolha a opção: "))
+        match op:
+            case 1:
+                df = pd.read_sql_query('SELECT * FROM api_produto', self.conn)
+                return print(df)
+            case 2:
+                valor = int(input('ID: '))
+                query = f'SELECT * FROM api_produto WHERE id = {valor}'
+                df = pd.read_sql_query(query, self.conn)
+                return print(df)
+            case _:
+                print("Escolha uma opção válida...")
+
+    def update(self, id, titulo=None, preco=None, descricao=None, imagemProduto=None,
+               categoriaProduto=None, classProduto=None, exibirHome=None):
+        cursor = self.conn.cursor()
+        campos = []
+        valores = []
+
+        if titulo:
+            campos.append('tituloProduto = ?')   
+            valores.append(titulo) 
+        if preco:
+            campos.append('preco = ?')   
+            valores.append(preco) 
+        if descricao:
+            campos.append('descricao = ?')   
+            valores.append(descricao) 
+        if imagemProduto:
+            campos.append('imagemProduto = ?')   
+            valores.append(imagemProduto) 
+        if categoriaProduto:
+            campos.append('categoriaProduto = ?')   
+            valores.append(categoriaProduto) 
+        if classProduto:
+            campos.append('classProduto = ?')   
+            valores.append(classProduto) 
+        if exibirHome is not None:
+            campos.append('exibirHome = ?')   
+            valores.append(exibirHome) 
+
+        if campos:
+            valores.append(id)
+            cursor.execute(f'''
+                UPDATE api_produto SET {', '.join(campos)} WHERE id = ?
+                ''', valores)
+            self.conn.commit()
+            print('Produto atualizado com sucesso...')
+        else:
+            print('Nenhum produto atualizado...')
+
 
 
 ecommerce = Ecommerce()
